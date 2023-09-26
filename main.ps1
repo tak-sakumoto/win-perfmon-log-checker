6a66db1da1b424e23d3958fbcfec9b709c75fd0b
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-09-27 00:02:05 +0900

    Fix processes for locations of the output files; fix processes for invalid chars in file names

--- main.ps1
+++ main.ps1
@@ -4,10 +4,14 @@ param (
     [string]$outDirPath = ".\out"
 )
 
+# Make a foloder to save output files
 New-Item -Path $outDirPath -ItemType Directory -Force
+
+# Get a file path for the CSV file to store all counters
 $allCSVPath = "$outDirPath\all.csv"
 
-$invalidChars = [IO.Path]::GetInvalidFileNameChars()
+# Search for invalid characters in file paths, excluding backslashes
+$invalidChars = [IO.Path]::GetInvalidFileNameChars() | Where-Object { $_ -ne "\\" }
 
 # Convert the specified blg file to a CSV file
 Start-Process -NoNewWindow -Wait -FilePath "relog.exe" -ArgumentList "$blgPath", "-f", "CSV", "-o", "$allCSVPath"
@@ -20,15 +24,22 @@ $counterNames = $csv | Get-Member -MemberType NoteProperty | Select-Object -Expa
 
 # Export a CSV file for each counter
 for ($i = 1; $i -lt $counterNames.Count; $i++) {
-    $counterName = $counterNames[$i]
+    # Get a name of a CSV file for storing a counter
+    $outFileName = $counterNames[$i]
+
+    # Replace invalid characters in the file name to underscores 
     foreach($char in $invalidChars) {
         $escapedChar = [Regex]::Escape($char)
         if($counterName -match $escapedChar) {
-            $outFileName = $counterName -replace $escapedChar,"_"
+            $outFileName = $outFileName -replace $escapedChar,"_"
         }
     }
-    $outFileName = $outFileName -replace "\\", " "
 
+    # Create the parent folders for the exported CSV file
+    $parentPath = Split-Path -Path $outDirPath\$outFileName -Parent
+    New-Item -Path $parentPath -ItemType Directory -Force -ErrorAction SilentlyContinue
+    
+    # Export the CSV file for the counter
     $outPath = "$outDirPath\$outFileName.csv"
     $csv | Select-Object -Property $counterNames[0], $counterNames[$i] | Export-Csv -Path $outPath -NoTypeInformation
 }
