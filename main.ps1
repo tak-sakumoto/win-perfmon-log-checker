602bbf7d4fdbb86de1e9b6c6485290e5c2011d5c
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-10-06 01:45:15 +0900

    Add to processes for drawing line graphs by Excel

--- main.ps1
+++ main.ps1
@@ -4,8 +4,14 @@ param (
     [string]$outDirPath = ".\out"
 )
 
+# Constants
+$chartHeight = 300
+$chartWidth = 400
+$chartType = 4 # 4: line graph
+
 # Make a foloder to save output files
 New-Item -Path $outDirPath -ItemType Directory -Force
+$outDirPath = Convert-Path $outDirPath
 
 # Get a file path for the CSV file to store all counters
 $allCSVPath = "$outDirPath\all.csv"
@@ -37,11 +43,55 @@ for ($i = 1; $i -lt $counterNames.Count; $i++) {
         }
     }
 
+    # Remove duplicate backslashes
+    $outFileName = $outFileName -replace '(\\{2,})', '\'
+
     # Create the parent folders for the exported CSV file
-    $parentPath = Split-Path -Path $outDirPath\$outFileName -Parent
+    $parentPath = Split-Path -Path $outDirPath$outFileName -Parent
     New-Item -Path $parentPath -ItemType Directory -Force -ErrorAction SilentlyContinue
     
     # Export the CSV file for the counter without unnecessary quotes
-    $outPath = "$outDirPath\$outFileName.csv"
+    $outPath = "$outDirPath$outFileName.csv"
     $csv | Select-Object -Property $counterNames[0], $counterNames[$i] | Export-Csv -Path $outPath -NoTypeInformation -UseQuotes AsNeeded
+
+    # Prepare to handle Excel
+    $excel = New-Object -ComObject Excel.Application
+    $excel.Visible = $false
+    $excel.DisplayAlerts = $false
+
+    # Prepare a workbook with a sheet
+    $workbook = $excel.Workbooks.Add()
+    $worksheet = $workbook.Worksheets.Item(1)
+
+    # Set column names to the 1st row of the workbook
+    $worksheet.Cells.Item(1, 1) = $counterNames[0]
+    $worksheet.Cells.Item(1, 2) = $counterNames[$i]
+
+    # Set data after the 2nd row of the workbook
+    $j = 2
+    foreach ($line in $csv) {
+        $worksheet.Cells.Item($j, 1) = $line.($counterNames[0])
+        $worksheet.Cells.Item($j, 2) = $line.($counterNames[$i])
+        $j++
+    }
+
+    # Add a chart in the workbook
+    $chart = $worksheet.Shapes.AddChart2(-1, $chartType, 0, 0, $chartWidth, $chartHeight, $true)
+
+    # Specify a range of data in the workbook
+    $dataRange = $worksheet.Range($worksheet.Cells.Item(1, 2), $worksheet.Cells.Item($j, 2))
+    $chart.Chart.SetSourceData($dataRange)
+
+    # Save the Excel book
+    $outPath = "$outDirPath$outFileName.xlsx"
+    $workbook.SaveAs($outPath)
+
+    $workbook.Close()
+    $excel.Quit()
+
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet)
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook)
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
+    [System.GC]::WaitForPendingFinalizers()
+    [System.GC]::Collect()
 }
