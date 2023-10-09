f0ae4f3d81a6da23a01381c2425118ed5128c7e7
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-10-09 14:50:35 +0900

    Move the graph drawing function to a separate script

--- main.ps1
+++ main.ps1
@@ -6,10 +6,8 @@ param (
     [string]$endTime = ""
 )
 
-# Constants
-$chartHeight = 300
-$chartWidth = 400
-$chartType = 4 # 4: line graph
+# Dot sourcing
+. .\save_graph_by_excel.ps1
 
 # Make a foloder to save output files
 New-Item -Path $outDirPath -ItemType Directory -Force
@@ -88,66 +86,8 @@ for ($i = 1; $i -lt $counterNames.Count; $i++) {
 
     # Export the stats array
     $stats | Select-Object Count, Maximum, Minimum, Average | Export-Csv -Path $outPath -NoTypeInformation -UseQuotes AsNeeded
-
-    # Prepare to handle Excel
-    $excel = New-Object -ComObject Excel.Application
-    $excel.Visible = $false
-    $excel.DisplayAlerts = $false
-
-    # Prepare a workbook with a sheet
-    $workbook = $excel.Workbooks.Add()
-    $worksheet = $workbook.Worksheets.Item(1)
-
-    # Set column names to the 1st row of the workbook
-    $worksheet.Cells.Item(1, 1) = $counterNames[0]
-    $worksheet.Cells.Item(1, 2) = $counterNames[$i]
-
-    # Set data after the 2nd row of the workbook
-    $j = 2
-    foreach ($line in $csv) {
-        $worksheet.Cells.Item($j, 1) = $line.($counterNames[0])
-        $worksheet.Cells.Item($j, 2) = $line.($counterNames[$i])
-        $j++
-    }
-
-    # Add a chart in the workbook
-    $chart = $worksheet.Shapes.AddChart2(-1, $chartType, 0, 0, $chartWidth, $chartHeight, $true)
-
-    # Specify a range of data in the workbook
-    $seriesCollection = $chart.Chart.SeriesCollection()
-    $series = $seriesCollection.NewSeries()
-    $series.XValues = $worksheet.Range($worksheet.Cells.Item(2, 1), $worksheet.Cells.Item($j, 1))
-    $series.Values = $worksheet.Range($worksheet.Cells.Item(2, 2), $worksheet.Cells.Item($j, 2))
-
-    # Get the axes
-    $xAxis = $chart.Chart.Axes(1, 1)
-    $yAxis = $chart.Chart.Axes(2, 1)
-
-    # Set the axis titles
-    $xAxis.HasTitle = $true
-    $xAxis.AxisTitle.Text = $worksheet.Cells.Item(1, 1)
-    $yAxis.HasTitle = $true
-    $yAxis.AxisTitle.Text = $worksheet.Cells.Item(1, 2)
-
-    # Save the Excel book
+    
+    # Draw a line graph for the counter and save as an Excel workbook
     $outPath = "$outDirPath$outFileName.xlsx"
-    $workbook.SaveAs($outPath)
-
-    $workbook.Close()
-    $excel.Quit()
-
-    # Release COM objects
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($yAxis) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($xAxis) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($series) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($seriesCollection) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($chart) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-
-    # Perform garbage collection
-    [System.GC]::Collect()
-    [System.GC]::WaitForPendingFinalizers()
-    [System.GC]::Collect()
+    Save-Graph-By-Excel -outPath $outPath -xAxisName $counterNames[0] -yAxisName $counterNames[$i] -csv $csv
 }
